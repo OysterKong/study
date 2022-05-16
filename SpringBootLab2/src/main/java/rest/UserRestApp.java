1b8147423b8ef79bfe660f5d4e826b9f9855dfe8
홍진우 <whatar@naver.com>
2022-05-16 14:33:59 +0900

    5월 16일 오전 - 테스트코드 ( insomnia ) 로 실습

--- SpringBootLab2/src/main/java/rest/UserRestApp.java
+++ SpringBootLab2/src/main/java/rest/UserRestApp.java
@@ -3,7 +3,7 @@ package rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //main클래스의 역할을 하도록 생성
 public class UserRestApp {
 	
 	public static void main(String [] args) {
@@ -15,7 +15,7 @@ public class UserRestApp {
 	 * rest 패키지 아래에 4개의 패키지 생성
 	 * -----------------------------
 	 * entity ( DTO 랑 역할이 같다 )
-	 * service 
+	 * service ( 반드시 있어야하는건 아니지만 의존성<직접적으로 연결되는 것을 막기 위한>, 유지보수를 위해... )
 	 * store ( DAO 랑 역할이 같다 )  ( INTERFACE 역할 )
 	 * controller
 	 */
--- SpringBootLab2/src/main/java/rest/controller/UserController.java
+++ SpringBootLab2/src/main/java/rest/controller/UserController.java
@@ -1,7 +1,13 @@
 package rest.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +23,29 @@ public class UserController {
 	//private final UserService userService;  //lombok일시
 	
 	@PostMapping("/users")
-	public String register(@RequestBody User newUser) {
+	public String register(@RequestBody User newUser) { //@RequestBody 는 많은 양의 데이터를 한번에 받기 위한 것(body에 담음) - post방식
 		return userService.register(newUser);
 	}
+	
+	@GetMapping("/users/{id}")
+	public User find(@PathVariable String id) {  //@PathVariable는  header에 데이터를 담아오면 꺼내오는, 즉 , url에 데이터를 담아 보내는 get방식
+		return userService.find(id);
+	}
+	
+	@GetMapping("/users")
+	public List<User> findAll(){
+		return userService.findAll();
+	}
+	
+	@PutMapping("/users") //@PutMapping 은 GetMapping 과 비슷한 방식, 실제 API 설계시 주소값을 바꾸는건 권장하지 않음
+	public void modify(@RequestBody User newUser) {
+		userService.modify(newUser);
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public void remove(@PathVariable String id) {
+		userService.remove(id);
+	}
+	
 	
 }
