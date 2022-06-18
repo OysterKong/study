d30eb1fe2e29a2933913401689fdbfb7190836a5
홍진우 <whatar@naver.com>
2022-06-18 21:15:19 +0900

    주말 자바 기초부터..

---
+++ Chapter3/src/ch13/domain/userinfo/UserInfo.java
@@ -0,0 +1,31 @@
+package ch13.domain.userinfo;
+
+public class UserInfo {
+	
+	private String userid;
+	private String password;
+	private String userName;
+	
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+	
+
+}
